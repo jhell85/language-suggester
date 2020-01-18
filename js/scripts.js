555ff7219301f62456af0ab8a043e7d0b6f308be
josh <jhell85@gmail.com>
2020-01-17 22:50:28 -0800

    finished question 5 HTML, changed variable structure in js

--- js/scripts.js
+++ js/scripts.js
@@ -3,75 +3,91 @@ $(document).ready(function() {
     $("#startContainer").addClass("none")
     $("#question1").removeClass("none")
   })
-  var q1;
-  var q2;
-  var q3;
-  var q4;
-  var q5;
+  var python = 0;
+  var java = 0;
+  var c = 0;
+  
 
   $("#q1o1").click(function () {
     $("#question1").addClass("none")
-    q1 = "JavaScript"
+    java += 1
     $("#question2").removeClass("none")
   })
   $("#q1o2").click(function () {
     $("#question1").addClass("none")
-    q1 = "C#"
+    c += 1
     $("#question2").removeClass("none")
   })
   $("#q1o3").click(function () {
     $("#question1").addClass("none")
-    q1 = "Python"
+    python += 1
     $("#question2").removeClass("none")
+    console.log(python, c, java)
   })
 
   $("#q2o1").click(function () {
     $("#question2").addClass("none")
-    q2 = "Python"
+    python = 1
     $("#question3").removeClass("none")
   })
   $("#q2o2").click(function () {
     $("#question2").addClass("none")
-    q2 = "JavaScript"
+    java = 1
     $("#question3").removeClass("none")
   })
   $("#q2o3").click(function () {
     $("#question2").addClass("none")
-    q2 = "C#"
+    c = 1
     $("#question3").removeClass("none")
+    console.log(python, c, java)
   })
   
   $("#q3o1").click(function () {
     $("#question3").addClass("none")
-    q3 = "C#"
+    c += 1
     $("#question4").removeClass("none")
   })
   $("#q3o2").click(function () {
     $("#question3").addClass("none")
-    q3 = "Python"
+    python += 1
     $("#question4").removeClass("none")
   })
   $("#q3o3").click(function () {
     $("#question3").addClass("none")
-    q3 = "JavaScript"
+    java += 1
     $("#question4").removeClass("none")
   })
 
   $("#q4o1").click(function () {
     $("#question4").addClass("none")
-    q2 = "C#"
+    c += 1
     $("#question5").removeClass("none")
   })
   $("#q4o2").click(function () {
     $("#question4").addClass("none")
-    q2 = "Python"
+    python += 1
     $("#question5").removeClass("none")
   })
   $("#q4o3").click(function () {
     $("#question4").addClass("none")
-    q2 = "JavaScript"
+    java += 1
     $("#question5").removeClass("none")
   })
 
+  $("#q5o1").click(function () {
+    $("#question5").addClass("none")
+    c += 1
+    console.log(python, c, java)
+  })
+  $("#q5o2").click(function () {
+    $("#question5").addClass("none")
+    python += 1
+    console.log(python, c, java)
+  })
+  $("#q5o3").click(function () {
+    $("#question5").addClass("none")
+    java += 1
+    console.log(python, c, java)
+  })
 
 })
