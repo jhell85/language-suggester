5d950e00d9d300d587885fd019986cbb9be79868
josh <jhell85@gmail.com>
2020-01-17 16:25:26 -0800

    set responses to question 1 to variable

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,22 @@ $(document).ready(function() {
   var q3;
   var q4;
   var q5;
-  
 
-
+  $("#q1o1").click(function () {
+    $("#question1").addClass("none")
+    q1 = "JavaScript"
+    
+  })
+  $("#q1o2").click(function () {
+    $("#question1").addClass("none")
+    q1 = "C#"
+    
+  })
+  $("#q1o3").click(function () {
+    $("#question1").addClass("none")
+    q1 = "Python"
+    
+  })
 
 
 
