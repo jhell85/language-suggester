35bd5328d7f84e39b543597d069c64f6acc5e5db
josh <jhell85@gmail.com>
2020-01-17 17:36:45 -0800

    add Jquery to question 2

--- js/scripts.js
+++ js/scripts.js
@@ -24,7 +24,18 @@ $(document).ready(function() {
     q1 = "Python"
     $("#question2").removeClass("none")
   })
- 
+  $("#q2o1").click(function () {
+    $("#question2").addClass("none")
+    q2 = "Python"
+  })
+  $("#q2o2").click(function () {
+    $("#question2").addClass("none")
+    q2 = "JavaScript"
+  })
+  $("#q2o3").click(function () {
+    $("#question2").addClass("none")
+    q2 = "C#"
+  })
 
 
 })
