2165fd0af1de68e6d887e8c57729f73db1e7fb40
josh <jhell85@gmail.com>
2020-01-17 16:08:14 -0800

    added more choices for question 1

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,13 @@ $(document).ready(function() {
     $("#startContainer").addClass("none")
     $("#question1").removeClass("none")
   })
+  var q1;
+  var q2;
+  var q3;
+  var q4;
+  var q5;
+  
+
 
 
 
